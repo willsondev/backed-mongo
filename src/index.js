da50d39bf997fc82a41b309c9e1987a19babd7e3
willsondev <willsondev12@gmail.com>
2024-09-16 18:58:08 -0300

    ip desde atals

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => {
     logger.info('MongoDB connected'); // Registra la conexión en el log
     app.listen(port, () => {
-      logger.info(`Server running on port ${port}`); // Registra el inicio del servidor en el log
+      logger.info(`Server corriendo on port ${port}`); // Registra el inicio del servidor en el log
     });
   })
   .catch(err => logger.error('Error connecting to MongoDB:', err)); // Registra el error de conexión en el log
